141ef88843e98874871ac06f6b72106e92332c97
Youssef <ya103857@gmail.com>
2026-05-25 15:22:47 +0300

    Add files via upload

---
+++ Python Exercise (7).py
@@ -0,0 +1,19 @@
+name = "Youssef"
+age = 16
+country = "Eygpt"
+
+print("My name is" , name)
+print("I am ", age ,"years old")
+print("I live in", country)
+print ("*"*30)
+
+name = input("What is your name?: ")
+
+age = input("How old are you?: ")
+
+country = input("What is your country?: ")
+
+print ("My name is ",name)
+print ("I am",age,"years old")
+print ("I live in",country)
+
